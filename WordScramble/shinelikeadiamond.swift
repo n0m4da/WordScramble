ec4bc7b02e5d910bb90567b03cacd1cfdbb9fe5f
n0m4da <nomad._@hotmail.com>
2023-12-17 22:05:42 -0700

    control error

---
+++ WordScramble/shinelikeadiamond.swift
@@ -0,0 +1,18 @@
+//
+//  shinelikeadiamond.swift
+//  WordScramble
+//
+//  Created by Luis Rivera on 16/12/23.
+//
+
+import SwiftUI
+
+struct shinelikeadiamond: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    shinelikeadiamond()
+}
